0ee4aedbb05f5325ed37434034f6a986c28107a8
a a <a@gmail.com>
2026-06-05 00:28:42 -0700

    init lock lab

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -28,11 +28,14 @@ kinit()
 {
   initlock(&kmem.lock, "kmem");
   freerange(end, (void*)PHYSTOP);
+
+  // initialize 8 free lists
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
+  // TODO : give all free mem to cpu
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
@@ -46,6 +49,10 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+
+  // TODO : determine which cpu is calling (push_off, pop_off)
+  //        append free *pa to corresponding list
+
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
